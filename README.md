3e52bb0640cc1b63b4ff46a60bb0f29c78e1cec4
Cynthia Deng <cynthia.deng@ubc.ca>
2018-04-23 11:04:39 -0700

    UPDATE ReadMe

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# Ubc-rest-post-meta
-For Wordpress:
-Modifying REST API fields to include post meta data
+# UBC Post Meta Data (Custom Fields)
+Tags: UBC CMS
+
+This plugin uses WP REST API to add a custom endpoint to retrieve custom field values from posts, not including hidden post metadata.
+
+How to use: 
+- [Required] To grab all custom fields attached to a specified post, add the following to the end of your website URL:  /?rest_route=/postmeta/v1/fields/{POST_ID} 
+
+- [Optional] To grab the values of a single custom field, add the following to the end of your website URL:   /?rest_route=/postmeta/v1/fields/{POST_ID}/{CUSTOM_FIELD_KEY} 
--- ubc-rest-post-meta.php
+++ ubc-rest-post-meta.php
@@ -2,9 +2,9 @@
 /**
  *
  * @wordpress-plugin
- * Plugin Name:       REST API - Custom Endpoint for Post Meta
+ * Plugin Name:       UBC Post Meta Data (Custom Fields)
  * Description:       Adding Custom Endpoint to retrieve custom field values from post meta data
- * Version:           1.0.0.2
+ * Version:           1.0.0
  * Author:            Cynthia Deng 
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
@@ -52,7 +52,7 @@ class UBC_WP_REST_API_meta extends WP_REST_Controller {
         }
 
         //If field key specified, get one custom field value
-        if ( isset( $object['fieldkey'] ) &&  ! empty( $object['fieldkey'] ) )  {
+        if ( isset( $object['fieldkey'] ) &&  ! empty( $object['fieldkey'] ) ) {
 
             $field_key = sanitize_title( $object['fieldkey'] );
 
@@ -62,7 +62,7 @@ class UBC_WP_REST_API_meta extends WP_REST_Controller {
 
             $value = get_post_meta( $post_id, $field_key );
 
-            } else {
+        } else {
             
             //No field key specified, get all public data
             $custom_field = get_post_meta( $post_id, '', false );
@@ -73,7 +73,8 @@ class UBC_WP_REST_API_meta extends WP_REST_Controller {
 
                 $pos = strpos( $key, $hidden );
 
-                if ( strpos( $key, $hidden ) === 0 ){
+                //check if private metadata
+                if ( $pos === 0 ) {
                     unset( $custom_field[ $key ] );
                 }
                 
